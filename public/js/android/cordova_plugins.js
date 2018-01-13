06d3020d736b63a9b4c7e84a1b2c0842ce573840
wvictor <wvictor@hk1.ibm.com>
2018-01-13 11:22:34 +0800

    Added the app version checking

--- public/js/android/cordova_plugins.js
+++ public/js/android/cordova_plugins.js
@@ -41,6 +41,14 @@ module.exports = [
         "clobbers": [
             "Connection"
         ]
+    },
+    {
+        "id": "cordova-plugin-appversion.RareloopAppVersion",
+        "file": "plugins/cordova-plugin-appversion/www/app-version.js",
+        "pluginId": "cordova-plugin-appversion",
+        "clobbers": [
+            "AppVersion"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -51,7 +59,8 @@ module.exports.metadata =
     "cordova-plugin-qrscanner": "2.5.0",
     "cordova-plugin-whitelist": "1.3.3",
     "cordova-plugin-device": "2.0.1",
-    "cordova-plugin-network-information": "2.0.1"
+    "cordova-plugin-network-information": "2.0.1",
+    "cordova-plugin-appversion": "1.0.0"
 };
 // BOTTOM OF METADATA
 });
